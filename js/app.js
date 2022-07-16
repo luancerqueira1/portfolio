25134ced7f79ff1fd7b509ac233f5a3238720312
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-07-16 12:50:51 -0300

    ainda fazendo o nav

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 const menuBtn = document.querySelector('.menu-btn');
 const header = document.querySelector('.header-content');
+const navLinkActive = document.querySelector('.navbar-link');
 
 menuBtn.onclick = () => {
    header.classList.toggle('active');
-   menuBtn.classList.add('active');
+   menuBtn.classList.toggle('active');
 }
+
+navLinkActive.forEach()
